e3ea298318bb23562969f68f73d809aed5f8330e
Isaiah2am <isaiahkoroma@hotmail.com>
2025-05-13 12:32:57 +0200

    01

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace M4Flex_netmvc_main.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ eersteproject/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine(1);
+Console.WriteLine("mario");
+Console.WriteLine(false);
+Console.WriteLine($"{1}, {4}");
