af4768bc64b5176cf9532874da464cc086a37551
Isaiah2am <isaiahkoroma@hotmail.com>
2025-06-05 12:52:58 +0200

    FLEX

--- week1/mvc/Controllers/HomeController.cs
+++ week1/mvc/Controllers/HomeController.cs
@@ -18,9 +18,15 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult Index()
     {
-        return View();
+        Newsitem item = new Newsitem
+        {
+            Title = "Welcome to M4Flex",
+            Content = "This is a sample news item for the M4Flex application.",
+            ImageUrl = "img/news1.jpg"
+        };
+        return View(item);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
